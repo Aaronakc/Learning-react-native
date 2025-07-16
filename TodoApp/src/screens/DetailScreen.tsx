655b3f0b85f412db3539c87a7ed060d6f150f5ff
Aarona KC <aaronakc99@gmail.com>
2025-07-16 21:42:20 +0545

    implemented the edit functionality in todo

--- TodoApp/src/screens/DetailScreen.tsx
+++ TodoApp/src/screens/DetailScreen.tsx
@@ -1,30 +1,59 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Pressable, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types/navigation';
-import { useAppSelector } from '../store/Hooks';
+import { useAppDispatch, useAppSelector } from '../store/Hooks';
 import { RootState } from '../store/store';
+import { saveEdit, startEdit } from '../store/todoSlice';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'DetailScreen'>;
 
 const DetailScreen = ({ route, navigation }: Props) => {
-  const  index  = route.params?.index;
+  const index = route.params?.index;
   const { todos } = useAppSelector((state: RootState) => state.todo);
+  const dispatch = useAppDispatch()
   const item = todos[index];
+
+  const edit = item.edit;
+
+  const [title, setTitle] = useState(item?.title || '')
+  const [description, setDescription] = useState(item?.description || '')
+  const [date, setDate] = useState(item?.date || '')
+
+
+  const handleEdit = (index: number) => {
+    dispatch(startEdit(index))
+
+  }
+
+  const handleSave = (index: number, title: string, description: string, date: string) => {
+    const editedText = {
+      index,
+      title,
+      description,
+      date,
+    }
+    dispatch(saveEdit(editedText))
+
+
+
+  }
+
 
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Details</Text>
       {item ? (
         <View style={styles.wrapper}>
-          <View>
-            <Text style={styles.font}>Title: {item.title}</Text>
-            <Text style={styles.font}>Description: {item.description}</Text>
-            <Text style={styles.font}>Date: {item.date}</Text>
+          <View style={{ flex: 1 }}>
+            <View style={styles.font}> {edit ? <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)} /> : <Text>Title:{item?.title}</Text>}</View>
+            <View style={styles.font}>  {edit ? <TextInput style={styles.input} value={description} onChangeText={(text) => setDescription(text)} /> : <Text>Description:{item?.description}</Text>}</View>
+            <View style={styles.font}>  {edit ? <TextInput style={styles.input} value={date} onChangeText={(text) => setDate(text)} /> : <Text>Date:{item?.date}</Text>}</View>
+
           </View>
           <View>
-            <TouchableOpacity>
-              <Image source={require('../../assets/editIcon.png')} style={styles.icon} />
+            <TouchableOpacity onPress={edit ? () => handleSave(index, title, description, date) : () => handleEdit(index)} >
+              <Image source={edit ? require('../../assets/saveIcon.png') : require('../../assets/editIcon.png')} style={styles.icon} />
             </TouchableOpacity>
           </View>
         </View>
@@ -66,6 +95,7 @@ const styles = StyleSheet.create({
   font: {
     fontFamily: 'serif',
     paddingHorizontal: 15,
+    // width:"100%",
   },
   heading: {
     fontFamily: 'serif',
@@ -78,6 +108,14 @@ const styles = StyleSheet.create({
     width: 15,
     height: 15,
   },
+  input: {
+    borderRadius: 10,
+    borderWidth: 1,
+    marginVertical: 10,
+
+   
+
+  }
 });
 
 export default DetailScreen;
--- TodoApp/src/store/todoSlice.ts
+++ TodoApp/src/store/todoSlice.ts
@@ -34,10 +34,25 @@ export const todoSlice = createSlice({
     if(state.todos[index])
       state.todos[index].checked=!state.todos[index].checked
     },
+    startEdit:(state,action:PayloadAction<number>)=>{
+    const index=action.payload
+    if(state.todos[index])
+      state.todos[index].edit= true
+    },
+
+    saveEdit:(state,action:PayloadAction<{index:number;title:string;description:string,date:string}>)=>{
+      const {index,title,description,date}=action.payload;
+      if(state.todos[index]){
+        state.todos[index]={
+          ...state.todos[index],title,description,date,edit:false,
+        }
+      }
+
+    }
   
   },
 })
 
-export const { addTodo ,deleteTodo,toggleTodo} = todoSlice.actions
+export const { addTodo ,deleteTodo,toggleTodo, startEdit,saveEdit} = todoSlice.actions
 
 export default todoSlice.reducer
